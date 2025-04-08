b5227cbc1bc5cc66004870f5fd4d036dda7be306
Brunicoo <brunoconvaliarejas7@gmail.com>
2025-04-08 14:59:39 +0200

    changes ready

---
+++ NaivyBeatsApi/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using NaivyBeatsApi.Models;
+using System;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace NaivyBeatsApi.Controllers
+{
+    public class ChatController : ApiController
+    {
+        private NaivyBeatsEntities db = new NaivyBeatsEntities();
+
+
+        // POST: api/Chat
+        [ResponseType(typeof(bool))]
+        public bool newChat(Chat chat)
+        {
+            Chat c = new Chat();
+            
+            c.creation_date = DateTime.Now.Date.ToString("yyyy-MM-dd");
+            c.musician_id = chat.musician_id;
+            c.restaurant_id = chat.restaurant_id;
+
+            db.Chat.Add(c);
+            db.SaveChanges();
+
+            return true;
+        }
+    }
+}
---
+++ NaivyBeatsApi/Controllers/MessageController.cs
@@ -0,0 +1,29 @@
+﻿using NaivyBeatsApi.Models;
+using System;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace NaivyBeatsApi.Controllers
+{
+    public class MessageController : ApiController
+    {
+        private NaivyBeatsEntities db = new NaivyBeatsEntities();
+
+        // POST: api/Message
+        [ResponseType(typeof(bool))]
+        public bool newMessage(Message message)
+        {
+            Message m = new Message();
+
+            m.text = message.text;
+            m.publish_date = DateTime.Now.Date.ToString("yyyy-MM-dd");
+            m.chat_id = message.chat_id;
+            m.user_id = message.user_id;
+
+            db.Message.Add(m);
+            db.SaveChanges();
+
+            return true;
+        }
+    }
+}
